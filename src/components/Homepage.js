a60f22d6885478745bee2ee1bdc1fa26b714d86c
Ben Herbert <benherbert1983@googlemail.com>
2023-11-02 18:39:52 +0100

    Homepage basiscs

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,19 @@
 import './Homepage.scss'
+import { Link } from 'react-router-dom';
 
 const Homepage = () => {
     return (
-        <main>Main</main>
+        <main>
+            <section>
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <Link to="/bookingpage"><button aria-label='On Click'>Reserve a Table</button></Link>
+                <div>
+                    <img src={require('../img/restauranfood.jpg')} alt="Waiter with food"/>
+                </div>
+            </section>
+        </main>
     )
 }
 
